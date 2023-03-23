8ab09b9ecffa9cab5ae7b74834c23f97caf49462
Francesco-tot <juanfrancescogarcia@gmail.com>
2023-03-23 10:34:27 -0500

    Commit for divide in functions the request operations

--- store.py
+++ store.py
@@ -1,6 +1,20 @@
 import requests
 
+def request(URL : str):
+    return requests.get(URL)
+
+def get_status(URL : str):
+    r = request(URL)
+    return r.status_code
+
+def read_request_json(URL : str):
+    r = request(URL)
+    return r.json()
+
 def get_categories():
-    r = requests.get('https://api.escuelajs.co/api/v1/categories')
-    #print(r.status_code)
-    print(r.text)
+    r = request('https://api.escuelajs.co/api/v1/categories')
+    if get_status('https://api.escuelajs.co/api/v1/categories') != 200:
+        raise Exception("Conecction error")
+    jsonr = read_request_json('https://api.escuelajs.co/api/v1/categories')
+    for category in jsonr:
+        print(category['name'])
