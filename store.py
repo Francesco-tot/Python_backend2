34e100a86cc567ff7c79d2a9aabdb7e17a93ba70
Francesco-tot <juanfrancescogarcia@gmail.com>
2023-03-23 19:47:19 -0500

    Commit for some documents in the directory

--- store.py
+++ store.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 def request(URL : str):
     return requests.get(URL)
@@ -35,3 +36,17 @@ def SayHello():
 @app.get("/game/RPS")
 def initRPSGame():
     return f'Starting the game...'
+
+@app.get("/game",response_class = HTMLResponse)
+async def game_home_start():
+    return """<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to gamers place !!!</title>
+</head>
+<body>
+    <h1>Enjoy here with the games you can find :D</h1>
+</body>
+</html>"""
